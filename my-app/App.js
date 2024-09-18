150c7fd8fb87f10ac1006fd38af31d7f35614e31
Theo3011 <44906882+Theo3011@users.noreply.github.com>
2024-09-18 14:50:06 +0200

    s
    s

--- my-app/App.js
+++ my-app/App.js
@@ -1,5 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { getApps, initializeApp } from "firebase/app";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLRbQdp6oSPbiceBcR1JlrYQmIBGb52_0",
+  authDomain: "inno-95b61.firebaseapp.com",
+  projectId: "inno-95b61",
+  storageBucket: "inno-95b61.appspot.com",
+  messagingSenderId: "1043791623724",
+  appId: "1:1043791623724:web:fb40e9d8ceb06df9abc389",
+};
 
 export default function App() {
   return (
@@ -13,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ my-app/components/Add_edit_Car.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const FirstComponent = () => {
+  return (
+    <View>
+      <text> en to tre fire</text>
+    </View>
+  );
+};
+
+export default FirstComponent;
